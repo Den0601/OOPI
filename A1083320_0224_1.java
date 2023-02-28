205a89ebc598e31e6c51c4b7ae5fa92c65ea50b5
黃芊蝀 <61945850+Den0601@users.noreply.github.com>
2023-02-28 22:30:45 +0800

    Add files via upload

---
+++ A1083320_0224_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A1083320_0224_1{
+    public static void main (String[] args){
+    System.out.print("請輸入整數:");
+    Scanner input=new Scanner(System.in);
+    int i=input.nextInt();
+    input.close();
+    if(i%2==0){
+        System.out.println("此數字為偶數");
+    }else{
+        System.out.println("此數字為奇數");
+    }
+    }
+    }
---
+++ A1083320_0224_2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class A1083320_0224_2 {
+    public static void main (String[] args){
+        System.out.print("請輸入攝氏溫度:");
+        Scanner input=new Scanner(System.in);
+        int i=input.nextInt();
+        int a=(i*9/5)+32;
+        System.out.println("華氏溫度為:"+a+"度");
+    }
+}
